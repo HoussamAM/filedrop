70debcbd83ebea78610d9a6b0ca7d38e068886f6
HoussamAM <houssam.aitmoulay17@gmail.com>
2026-03-28 19:39:00 +0000

    update CORS for production

--- server/index.js
+++ server/index.js
@@ -10,7 +10,12 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://filedrop-5vumrh2zl-houssamams-projects.vercel.app'
+    ]
+}))
 app.use(express.json())
 
 // Routes
